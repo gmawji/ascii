541fb4ce13d176ccd06a79e21056f8a60d32a1de
jokerg7 <git@gmawji.com>
2017-01-06 15:16:16 -0500

    Added self.render("front.html")

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class Handler(webapp2.RequestHandler):
 
 class MainHandler(Handler):
     def get(self):
-        self.write("asciichan!")
+        self.render("front.html")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
